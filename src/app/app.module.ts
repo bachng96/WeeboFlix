4c8ae8583ca3af10be073172c62f269d7585d482
Nguyen Gia Bach <bachng96@gmail.com>
2022-05-24 10:03:38 +0700

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AnimeListComponent } from './components/anime-list/anime-list.component';
+import { AnimeDetailComponent } from './components/anime-detail/anime-detail.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimeListComponent,
+    AnimeDetailComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
