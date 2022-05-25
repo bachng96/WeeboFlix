6ad52480d6671c8c4feef2629ffa8c231c710da3
Hieu Khac <khacminhhieu99@gmail.com>
2022-05-25 10:52:42 +0700

    geresbox-ok

--- src/app/core/services/anime.service.ts
+++ src/app/core/services/anime.service.ts
@@ -1,14 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnimeService {
-  baseURL = 'https://api.jikan.moe/v4/';
+  private BASE_URL = 'https://api.jikan.moe/v4/';
+
   constructor(private http: HttpClient) {}
 
+  getAllAnime() {
+    return this.http.get(this.BASE_URL + 'anime').pipe();
+  }
+
   getAllGenres() {
-    return this.http.get(`${this.baseURL}genres/anime`);
+    return this.http.get(this.BASE_URL + 'genres/anime').pipe(
+      map((anime) => {
+        let keys = ['mal_id', 'name'];
+        let newSet = anime['data'].filter(
+          (
+            (s) => (o) =>
+              ((k) => !s.has(k) && s.add(k))(keys.map((k) => o[k]).join('|'))
+          )(new Set())
+        );
+        return newSet.sort((a, b) => a.name.localeCompare(b.name));
+      })
+    );
   }
 }
--- src/app/layout/genres-box/genres-box.component.ts
+++ src/app/layout/genres-box/genres-box.component.ts
@@ -8,11 +8,26 @@ import { AnimeService } from 'src/app/core/services/anime.service';
 })
 export class GenresBoxComponent implements OnInit {
   public genres;
+  public partGenres;
+  public show = false;
   constructor(private animeService: AnimeService) {}
 
   ngOnInit(): void {
     this.animeService.getAllGenres().subscribe((p) => {
       this.genres = p;
+      this.partGenres = this.genres.slice(0, 24);
+      console.log(this.partGenres);
     });
+  }
+
+  showAll() {
+    if (this.show == false) {
+      this.partGenres = this.genres.slice(0, 24);
+      this.show = !this.show;
+    } else {
+      this.partGenres = this.genres;
+      console.log(this.partGenres);
+      this.show = !this.show;
+    }
   }
 }
