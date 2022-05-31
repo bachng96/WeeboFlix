fe54c8ff2bd077ecd71385e38dff9ef56740bc45
Nguyen Gia Bach <bachng96@gmail.com>
2022-05-31 09:34:14 +0700

    add data to carosel

--- src/app/components/watch/watch.component.ts
+++ src/app/components/watch/watch.component.ts
@@ -19,6 +19,10 @@ export class WatchComponent implements OnInit {
       this.animeService.getAnimeById(params.id).subscribe(data => {
         console.log(data['data'])
         this.animeDetail = data['data'];
+        if(!this.animeDetail.trailer.embed_url){
+          console.log("when no trailer is found")
+          console.log(this.sanitizer.bypassSecurityTrustResourceUrl(this.animeDetail.trailer.embed_url)) 
+        }
       })
     })
   }
