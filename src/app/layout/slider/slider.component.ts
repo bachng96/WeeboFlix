69e497f812f94fc848af6df3365286151a2bbcbd
Nguyen Gia Bach <bachng96@gmail.com>
2022-06-02 15:10:44 +0700

    slider responsive

--- src/app/layout/slider/slider.component.ts
+++ src/app/layout/slider/slider.component.ts
@@ -18,6 +18,31 @@ SwiperCore.use([Pagination, Navigation]);
 export class SliderComponent implements OnInit {
   @Input() animes: Anime[];
 
+  swiperConfig: any = {
+    direction: 'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 40,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: false,
+    keyboard: true,
+    mousewheel: true,
+    breakpoints: {
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 50,
+      },
+    },
+  };
+
   constructor(private animeService: AnimeService) {}
 
   ngOnInit(): void {}
